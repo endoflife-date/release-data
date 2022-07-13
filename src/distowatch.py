e018736fbe562d75ff465f6c6ff4cb83b2646b63
Nemo <commits@captnemo.in>
2022-07-13 13:21:53 +0530

    [distrowatch] Support running single product

--- src/distowatch.py
+++ src/distowatch.py
@@ -1,6 +1,7 @@
 from glob import glob
 import os
 import re
+import sys
 import json
 import frontmatter
 import urllib.request
@@ -37,9 +38,11 @@ def fetch_releases(distrowatch_id, regex, template):
               releases[v] = date
     return releases
 
-def update_releases():
+def update_releases(product_filter=None):
   for product_file in glob("website/products/*.md"):
       product_name = os.path.splitext(os.path.basename(product_file))[0]
+      if product_filter and product_name != product_filter:
+        continue
       with open(product_file, "r") as f:
           data = frontmatter.load(f)
           if "auto" in data:
@@ -58,4 +61,7 @@ def update_product(product_name, config):
       print("::endgroup::")
 
 if __name__ == "__main__":
-  update_releases()
+  if len(sys.argv) > 1:
+    update_releases(sys.argv[1])
+  else:
+    update_releases()
