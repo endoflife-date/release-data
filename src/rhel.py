bf7b768c23225f07155229e85d645b0061620d85
Nemo <commits@captnemo.in>
2022-10-16 13:59:39 +0530

    [py] Fix styling

--- src/rhel.py
+++ src/rhel.py
@@ -8,24 +8,22 @@ URL = "https://access.redhat.com/articles/3078"
 regex = r"RHEL (?P<major>\d)(\. ?(?P<minor>\d+))?(( Update (?P<minor2>\d))| GA)?"
 
 list = {}
-headers = {
-  'user-agent': 'mozilla'
-}
-req = urllib.request.Request(URL,headers=headers)
+headers = {"user-agent": "mozilla"}
+req = urllib.request.Request(URL, headers=headers)
 
 with urllib.request.urlopen(req, timeout=5) as response:
-  soup = BeautifulSoup(response, features="html5lib")
-  for tr in soup.findAll('tr'):
-    td_list = tr.findAll('td')
-    if len(td_list)>0:
-      version = td_list[0].get_text()
-      m = re.match(regex, version.strip()).groupdict()
-      version = m['major']
-      if m['minor']:
-        version += ".%s" % m['minor']
-      if m['minor2']:
-        version += ".%s" % m['minor2']
-      list[version] = td_list[1].get_text()
+    soup = BeautifulSoup(response, features="html5lib")
+    for tr in soup.findAll("tr"):
+        td_list = tr.findAll("td")
+        if len(td_list) > 0:
+            version = td_list[0].get_text()
+            m = re.match(regex, version.strip()).groupdict()
+            version = m["major"]
+            if m["minor"]:
+                version += ".%s" % m["minor"]
+            if m["minor2"]:
+                version += ".%s" % m["minor2"]
+            list[version] = td_list[1].get_text()
 
-with open('releases/redhat.json', 'w') as f:
-  f.write(json.dumps(list, indent=2))
+with open("releases/redhat.json", "w") as f:
+    f.write(json.dumps(list, indent=2))
--- src/ros.py
+++ src/ros.py
@@ -11,24 +11,26 @@ regex = r"^ROS (?P<name>(\w| )+)"
 list = {}
 
 with urllib.request.urlopen(URL, timeout=5) as response:
-  soup = BeautifulSoup(response, features="html5lib")
-  for tr in soup.findAll('tr'):
-    td_list = tr.findAll('td')
-    if len(td_list)>0:
-      version = td_list[0].get_text()
+    soup = BeautifulSoup(response, features="html5lib")
+    for tr in soup.findAll("tr"):
+        td_list = tr.findAll("td")
+        if len(td_list) > 0:
+            version = td_list[0].get_text()
 
-      m = re.match(regex, version.strip())
-      if m:
-        version = td_list[0].findAll('a')[0]['href'][1:]
-        try:
-          date = datetime.datetime.strptime(td_list[1].get_text().strip(), "%B %d, %Y")
-        # The date is a suffix (May 23rd, 2020)
-        except Exception as e:
-          x = td_list[1].get_text().split(',')
-          date = datetime.datetime.strptime(x[0][:-2] + x[1], "%B %d %Y")
-        abs_date = date.strftime("%Y-%m-%d")
-        list[version] = abs_date
-        print("%s: %s" % (version, abs_date))
+            m = re.match(regex, version.strip())
+            if m:
+                version = td_list[0].findAll("a")[0]["href"][1:]
+                try:
+                    date = datetime.datetime.strptime(
+                        td_list[1].get_text().strip(), "%B %d, %Y"
+                    )
+                # The date is a suffix (May 23rd, 2020)
+                except Exception as e:
+                    x = td_list[1].get_text().split(",")
+                    date = datetime.datetime.strptime(x[0][:-2] + x[1], "%B %d %Y")
+                abs_date = date.strftime("%Y-%m-%d")
+                list[version] = abs_date
+                print("%s: %s" % (version, abs_date))
 
-with open('releases/ros.json', 'w') as f:
-  f.write(json.dumps(list, indent=2))
+with open("releases/ros.json", "w") as f:
+    f.write(json.dumps(list, indent=2))
