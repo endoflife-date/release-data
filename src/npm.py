05b09772411189263ab4ba7fce32d68c234e9566
Rajat Jaiswal <76243388+anartify@users.noreply.github.com>
2022-12-16 17:08:24 +0530

    NPM automation (#36)
    Initial support for NPM Automation. This isn't widely used, since most NPM projects
    will use git tags as well, but helpful for a few projects.
    
    Co-authored-by: Hugo van Kemenade <hugovk@users.noreply.github.com>
    
    * Fix fstrings and remove unused imports
    
    * Fix the merge of product config with default config in npm.py and pypi.py
    
    If product config is first, default config overrides product config.
    
    * Simplify npm.py
    
    Co-authored-by: Nemo <me@captnemo.in>
    Co-authored-by: Hugo van Kemenade <hugovk@users.noreply.github.com>
    Co-authored-by: Marc Wrobel <marc.wrobel@gmail.com>

--- src/npm.py
+++ src/npm.py
@@ -5,34 +5,32 @@ import sys
 import json
 import frontmatter
 import urllib.request
-from datetime import datetime
 
-# Same as used in Ruby (update.rb)
 DEFAULT_TAG_TEMPLATE = (
     "{{major}}{% if minor %}.{{minor}}{% if patch %}.{{patch}}{%endif%}{%endif%}"
 )
 REGEX = r"^(?:(\d+\.(?:\d+\.)*\d+))$"
 
 
-def fetch_releases(pypi_id, regex):
+def fetch_releases(npm_id, regex):
     releases = {}
 
     if not isinstance(regex, list):
         regex = [regex]
 
-    url = "https://pypi.org/pypi/%s/json" % pypi_id
+    url = f"https://registry.npmjs.org/{npm_id}"
     with urllib.request.urlopen(url, data=None, timeout=5) as response:
         data = json.loads(response.read().decode("utf-8"))
-        for version in data["releases"]:
-            R = data["releases"][version]
+        for version in data["time"]:
             matches = False
             for r in regex:
                 if re.match(r, version):
                     matches = True
-            if matches and R:
-                d = datetime.fromisoformat(R[0]["upload_time"]).strftime("%Y-%m-%d")
-                releases[version] = d
-                print("%s: %s" % (version, d))
+
+            release_datetime = data["time"][version]
+            if matches and release_datetime:
+                releases[version] = release_datetime.split("T")[0]
+                print(f"{version}: {releases[version]}")
 
     return releases
 
@@ -42,23 +40,25 @@ def update_releases(product_filter=None):
         product_name = os.path.splitext(os.path.basename(product_file))[0]
         if product_filter and product_name != product_filter:
             continue
+
         with open(product_file, "r") as f:
             data = frontmatter.load(f)
             if "auto" in data:
                 for config in data["auto"]:
                     for key, d_id in config.items():
-                        if key == "pypi":
+                        if key == "npm":
                             update_product(product_name, config)
 
 
 def update_product(product_name, config):
-    if "pypi" in config:
-        print("::group::%s" % product_name)
+    if "npm" in config:
+        print(f"::group::{product_name}")
         config = {"regex": REGEX} | config
-        r = fetch_releases(config["pypi"], config["regex"])
-        with open("releases/%s.json" % product_name, "w") as f:
-            f.write(json.dumps(r, indent=2))
+        r = fetch_releases(config["npm"], config["regex"])
         print("::endgroup::")
+
+        with open(f"releases/{product_name}.json", "w") as f:
+            f.write(json.dumps(r, indent=2))
 
 
 if __name__ == "__main__":
