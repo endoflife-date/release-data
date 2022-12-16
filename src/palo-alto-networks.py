cc624a7cd4e3b0f69546be8b48101c80fc1e49f0
Nemo <commits@captnemo.in>
2022-12-16 16:40:11 +0530

    [pan] Fix new date ingestion

--- src/palo-alto-networks.py
+++ src/palo-alto-networks.py
@@ -1,6 +1,7 @@
 import json
 import urllib.request
 import datetime
+import re
 from bs4 import BeautifulSoup
 
 URL = "https://www.paloaltonetworks.com/services/support/end-of-life-announcements/end-of-life-summary"
@@ -28,14 +29,19 @@ def update_releases(html_identifier, file):
             version = version.removesuffix("-(cortex-xdr-agent)")
             version = version.removesuffix("-(vm-series-only)")
             version = version.removesuffix("-(panorama-only)")
-            try:
-                month, date, year = td_list[1].get_text().split("/")
-                abs_date = f"{year}-{month:0>2}-{date:0>2}"
-            except Exception:
-                date = datetime.datetime.strptime(td_list[1].get_text(), "%B %d, %Y")
-                abs_date = date.strftime("%Y-%m-%d")
+            if len(td_list) > 1 and version != "":
+                # Date formats differ between different products
+                try:
+                    month, date, year = td_list[1].get_text().split("/")
+                    abs_date = f"{year}-{month:0>2}-{date:0>2}"
+                except Exception:
+                    # A few dates have 1st, 2nd, 4th etc. Fix that:
+                    d = td_list[1].get_text()
+                    d = re.sub(r'(\w+) (\d{1,2})(?:\w{2}), (\d{4})', r'\1 \2, \3', d)
+                    date = datetime.datetime.strptime(d, "%B %d, %Y")
+                    abs_date = date.strftime("%Y-%m-%d")
 
-            versions[version] = abs_date
+                versions[version] = abs_date
 
     with open("releases/%s.json" % file, "w") as f:
         f.write(json.dumps(versions, indent=2))
