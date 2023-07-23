2505f7a819d3927642bfced9b68386cf09f5ea9e
Marc Wrobel <marc.wrobel@gmail.com>
2023-07-23 21:54:31 +0200

    Update script
    - use a for loop instead of kwargs, which I find way simpler to read and do npot necessitate additional loops for parsing the date and outputing the versions,
    - make the parsing of dates a function,
    - make the script fail for unknown date format (we want to be aware that something must be fixed).

--- src/apache.py
+++ src/apache.py
@@ -4,12 +4,26 @@ from pathlib import Path
 from common import endoflife
 from common.git import Git
 
-"""
-Fetch apache versions from the git repository
+"""Fetch apache versions from its git repository.
+
+Every branch formatted like 2.4.x has a STATUS file which contains a version
+history. Not every version was released, the lines are filtered with
+(?:Released|Announced) to get the released versions only (they were not
+consistent in the past with the wording, it seems better now).
 """
 
 PRODUCT = "apache"
 REPO_URL = "https://github.com/apache/httpd.git"
+
+
+def parse(date: str) -> str:
+    for format in ["%B %d, %Y", "%b %d, %Y"]:
+        try:
+            return datetime.strptime(date, format).strftime("%Y-%m-%d")
+        except ValueError:
+            pass
+
+    raise ValueError(f"Unknown date format for '{date}'")
 
 
 def get_versions_from_file(release_notes_file: Path) -> dict:
@@ -27,37 +41,19 @@ def get_versions_from_file(release_notes_file: Path) -> dict:
         )
     }
 
+
 git = Git(REPO_URL)
 git.setup()
 versions = {}
 
+print(f"::group::{PRODUCT}")
 for branch in git.list_branches("refs/heads/?.?.x"):
     status_file = "STATUS"
     git.checkout(branch, file_list=[status_file])
-    versions = {**versions, **get_versions_from_file(git.repo_dir / status_file)}
-
-# convert the date format
-converted = {}
-for version, date in versions.items():
-    try:
-        d = datetime.strptime(date, "%B %d, %Y")
-        converted[version] = d.strftime("%Y-%m-%d")
-        continue
-    except ValueError:
-        pass
-
-    try:
-        d = datetime.strptime(date, "%b %d, %Y")
-        converted[version] = d.strftime("%Y-%m-%d")
-        continue
-    except ValueError:
-        pass
-
-versions = converted
-
-print(f"::group::{PRODUCT}")
-for version, date in versions.items():
-    print(f"{version}: {date}")
+    for version, date_str in get_versions_from_file(git.repo_dir / status_file).items():
+        date = parse(date_str)
+        versions[version] = date
+        print(f"{version}: {date}")
 print("::endgroup::")
 
 endoflife.write_releases(
