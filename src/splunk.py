697b68928ebb8bc488c1ae8136e6a90f428b1df1
Marc Wrobel <marc.wrobel@gmail.com>
2023-08-01 23:15:08 +0200

    Refactor script

--- src/splunk.py
+++ src/splunk.py
@@ -6,54 +6,30 @@ from typing import List, Dict
 
 PRODUCT = "splunk"
 URL = "https://docs.splunk.com/Documentation/Splunk"
-RELEASE_NOTES_URL = "https://docs.splunk.com/Documentation/Splunk/{version}/ReleaseNotes/MeetSplunk"
-DATE_FORMAT = "%B %d, %Y"
+RELNOTES_URL_TEMPLATE = "https://docs.splunk.com/Documentation/Splunk/{release}/ReleaseNotes/MeetSplunk"
+PATTERN = r"Splunk Enterprise (?P<version>\d+\.\d+\.\d+(?:\.\d+)?) was released on (?P<date>\w+\s\d\d?,\s\d{4})\."
 
 
 def convert_date(date: str) -> str:
-    return datetime.strptime(date, DATE_FORMAT).strftime("%Y-%m-%d")
+    return datetime.strptime(date, "%B %d, %Y").strftime("%Y-%m-%d")
 
-def fetch_versions(url: str, min: str = '0') -> List[str]:
-    """
-    Get all the versions and return a list.
-    """
-    resp = endoflife.fetch_url(url)
-    soup = BeautifulSoup(resp, features="html5lib")
-    options = soup.select("select#version-select > option")
-
-    return sorted([
-        x.attrs['value']
-        for x in options
-        if 'value' in x.attrs and x.attrs['value'].split('.') >= min.split('.')
-    ], reverse=True, key=lambda x: x.split())
-
-def fetch_version_dates(version: str, release_url: str = RELEASE_NOTES_URL) -> Dict[str, str]:
-    """
-    Check the versions docs and extract all possible dates.
-    There may be dates for other versions too.
-    """
-    url = release_url.format(version=version)
-    resp = endoflife.fetch_url(url)
-
-    return {
-        version: convert_date(date)
-        for (version, date) in re.findall(
-            r"Splunk Enterprise (?P<version>\d+\.\d+\.\d+(?:\.\d+)?) was released on (?P<date>\w+\s\d\d?,\s\d{4})\.",
-            resp,
-            re.MULTILINE,
-        )
-    }
-
-releases = dict()
-# release data is only available for 7.2+
-for version in fetch_versions(URL, min='7.2'):
-    if version in releases:
-        continue
-    releases = {**releases, **fetch_version_dates(version)}
 
 print(f"::group::{PRODUCT}")
-for version, date in releases.items():
-    print(f"{version}: {date}")
+releases = dict()
+main = endoflife.fetch_url(URL)
+soup = BeautifulSoup(main, features="html5lib")
+
+for option in soup.select("select#version-select > option"):
+    minor_release = option.attrs['value']
+    # skip all releases before 7.2
+    if minor_release and minor_release.split('.') < '7.2'.split('.'):
+        continue
+
+    relnotes = endoflife.fetch_url(RELNOTES_URL_TEMPLATE.format(release=minor_release))
+    for (version, date_str) in re.findall(PATTERN, relnotes, re.MULTILINE):
+        date = convert_date(date_str)
+        releases[version] = date
+        print(f"{version}: {date}")
 
 endoflife.write_releases(PRODUCT, dict(
     # sort by version then date (asc)
