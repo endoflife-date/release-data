861a5a6d029584f645c13876f4e56fc803a0f593
Marc Wrobel <marc.wrobel@gmail.com>
2023-11-29 23:25:34 +0100

    [coldfusion] Fix script (#198)
    Initial release dates can't be scraped as they are not documented in release notes, they must be set manually.

--- src/coldfusion.py
+++ src/coldfusion.py
@@ -3,6 +3,16 @@ from bs4 import BeautifulSoup
 from common import dates
 from common import endoflife
 
+# Release dates are not available in the release notes, so we have to set them manually.
+RELEASE_DATES = {
+    "10.0.0": "2012-05-15", # https://en.wikipedia.org/wiki/Adobe_ColdFusion#Adobe_ColdFusion_10
+    "11.0.0": "2014-04-29", # https://en.wikipedia.org/wiki/Adobe_ColdFusion#Adobe_ColdFusion_11
+    "2016.0.0": "2016-02-16", # https://en.wikipedia.org/wiki/Adobe_ColdFusion#Adobe_ColdFusion_(2016_Release)
+    "2018.0.0": "2018-07-12", # https://coldfusion.adobe.com/2018/07/new-coldfusion-release-adds-performance-monitoring-toolset-for-measuring-monitoring-and-managing-high-performing-web-apps/
+    "2021.0.0": "2020-11-11", # https://community.adobe.com/t5/coldfusion-discussions/introducing-adobe-coldfusion-2021-release/m-p/11585468
+    "2023.0.0": "2022-05-16", # https://coldfusion.adobe.com/2023/05/coldfusion2023-release/
+}
+
 URLS = [
     "https://helpx.adobe.com/coldfusion/kb/coldfusion-10-updates.html",
     "https://helpx.adobe.com/coldfusion/kb/coldfusion-11-updates.html",
@@ -16,7 +26,7 @@ PRODUCT = "coldfusion"
 REGEX = r"[r|R]elease [d|D]ate[,|:]? (.*?)\).*?Build Number: (.*?)$"
 
 print(f"::group::{PRODUCT}")
-versions = {}
+versions = RELEASE_DATES | {}
 
 for response in endoflife.fetch_urls(URLS):
     soup = BeautifulSoup(response.text, features="html5lib")
