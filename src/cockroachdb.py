a4f64ceb88762d8fb2dec23436cd6ae442112b62
Rob Crowe <282571+rcrowe@users.noreply.github.com>
2024-02-25 15:56:49 +0000

    Reduce size of nested IF

--- src/cockroachdb.py
+++ src/cockroachdb.py
@@ -16,25 +16,26 @@ with releasedata.ProductData("cockroachdb") as product_data:
                 continue
 
             version_match = VERSION_REGEX.search(columns[0].text.strip())
-            if version_match:
-                release_version = version_match.group("version")
+            if not version_match:
+                continue
 
-                release = product_data.get_release(release_version)
-                release.set_release_date(dates.parse_date(columns[1].text))
-                release.set_support(dates.parse_date(columns[2].text))
-                release.set_eol(dates.parse_date(columns[3].text))
+            release_version = version_match.group("version")
+            release = product_data.get_release(release_version)
+            release.set_release_date(dates.parse_date(columns[1].text))
+            release.set_support(dates.parse_date(columns[2].text))
+            release.set_eol(dates.parse_date(columns[3].text))
 
-                # versions
-                release_response = http.fetch_url('https://www.cockroachlabs.com/docs/releases/v'+release_version)
-                release_soup = BeautifulSoup(release_response.text, features="html5lib")
+            # versions
+            release_response = http.fetch_url('https://www.cockroachlabs.com/docs/releases/v'+release_version)
+            release_soup = BeautifulSoup(release_response.text, features="html5lib")
 
-                content = release_soup.find(class_="post-content")
-                for version in content.find_all("h2"):
-                    if not version.find_next("p").text.startswith("Release Date"):
-                        continue
+            content = release_soup.find(class_="post-content")
+            for version in content.find_all("h2"):
+                if not version.find_next("p").text.startswith("Release Date"):
+                    continue
 
-                    version_release_date = version.find_next("p").text.removeprefix("Release Date:").strip()
-                    if len(version_release_date) == 0:
-                        continue
+                version_release_date = version.find_next("p").text.removeprefix("Release Date:").strip()
+                if len(version_release_date) == 0:
+                    continue
 
-                    product_data.declare_version(version.text, dates.parse_date(version_release_date))
+                product_data.declare_version(version.text, dates.parse_date(version_release_date))
