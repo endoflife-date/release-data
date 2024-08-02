464ca5ceee2e70ab78328262bb999cd1fbb5dbed
Marc Wrobel <marc.wrobel@gmail.com>
2024-08-02 20:31:53 +0200

    use releases instead of versions

--- src/kuma.py
+++ src/kuma.py
@@ -1,6 +1,5 @@
 import yaml
 from common import dates, http, releasedata
-from common.git import Git
 
 """Fetch version data for Kuma from https://raw.githubusercontent.com/kumahq/kuma/master/versions.yml
 """
@@ -8,13 +7,17 @@ from common.git import Git
 with releasedata.ProductData("kuma") as product_data:
     yml_response = http.fetch_url("https://raw.githubusercontent.com/kumahq/kuma/master/versions.yml")
     versions_data = yaml.safe_load(yml_response.text)
-    
+
     # Iterate through the versions and their associated dates
     for version_info in versions_data:
         if 'version' in version_info and 'releaseDate' in version_info:
-            version = version_info['version']
+            release_name = version_info['release'].replace('.x', '')
+            release = product_data.get_release(release_name)
 
-            date_str = version_info['releaseDate']
-            date = dates.parse_date(date_str)
-            
-            product_data.declare_version(version, date)
+            release_date_str = version_info['releaseDate']
+            release_date = dates.parse_date(release_date_str)
+            release.set_field('releaseDate', release_date)
+
+            eol_str = version_info['endOfLifeDate']
+            eol = dates.parse_date(eol_str)
+            release.set_field('eol', eol)
