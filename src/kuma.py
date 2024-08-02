d28daeae9b2823699dd7feaca3e2ef83560c66d2
Marc Wrobel <marc.wrobel@gmail.com>
2024-08-02 20:45:46 +0200

    improve script

--- src/kuma.py
+++ src/kuma.py
@@ -1,8 +1,14 @@
+import logging
+
 import yaml
 from common import dates, http, releasedata
 
-"""Fetch version data for Kuma from https://raw.githubusercontent.com/kumahq/kuma/master/versions.yml
+"""Fetch version data for Kuma from https://raw.githubusercontent.com/kumahq/kuma/master/versions.yml.
 """
+
+RELEASE_FIELD = 'release'
+RELEASE_DATE_FIELD = 'releaseDate'
+EOL_FIELD = 'endOfLifeDate'
 
 with releasedata.ProductData("kuma") as product_data:
     yml_response = http.fetch_url("https://raw.githubusercontent.com/kumahq/kuma/master/versions.yml")
@@ -10,14 +16,19 @@ with releasedata.ProductData("kuma") as product_data:
 
     # Iterate through the versions and their associated dates
     for version_info in versions_data:
-        if 'version' in version_info and 'releaseDate' in version_info:
-            release_name = version_info['release'].replace('.x', '')
-            release = product_data.get_release(release_name)
+        release_name = version_info[RELEASE_FIELD]
+        if not release_name.endswith('.x'):
+            logging.info(f"skipping release with name {release_name}: does not end with '.x'")
+            continue
 
-            release_date_str = version_info['releaseDate']
-            release_date = dates.parse_date(release_date_str)
-            release.set_field('releaseDate', release_date)
+        if RELEASE_DATE_FIELD not in version_info or EOL_FIELD not in version_info:
+            logging.info(f"skipping release with name {release_name}: does not contain {RELEASE_DATE_FIELD} or {EOL_FIELD} fields")
+            continue
 
-            eol_str = version_info['endOfLifeDate']
-            eol = dates.parse_date(eol_str)
-            release.set_field('eol', eol)
+        release = product_data.get_release(release_name.replace('.x', ''))
+
+        release_date = dates.parse_date(version_info[RELEASE_DATE_FIELD])
+        release.set_field('releaseDate', release_date)
+
+        eol = dates.parse_date(version_info[EOL_FIELD])
+        release.set_field('eol', eol)
