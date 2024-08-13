f238224681fccd3f8a9bd5049902e3d77561f8c1
Marc Wrobel <marc.wrobel@gmail.com>
2024-08-13 22:20:36 +0200

    [chef-inspec] Use GitHub releases instead of git tags (#367)
    GitHub releases gives better dates than git tags.

--- src/github_releases.py
+++ src/github_releases.py
@@ -1,9 +1,6 @@
-import json
-import logging
-import subprocess
 import sys
 
-from common import dates, endoflife, releasedata
+from common import dates, endoflife, github, releasedata
 
 """Fetches versions from GitHub releases using the GraphQL API and the GitHub CLI.
 
@@ -13,47 +10,17 @@ Note: GraphQL API and GitHub CLI are used because it's simpler: no need to manag
 METHOD = "github_releases"
 
 
-def fetch_releases(repo_id: str) -> list[dict]:
-    logging.info(f"fetching {repo_id} GitHub releases")
-    (owner, repo) = repo_id.split('/')
-    child = subprocess.run("""gh api graphql --paginate -f query='
-query($endCursor: String) {
-  repository(name: "%s", owner: "%s") {
-    releases(
-      orderBy: {field: NAME, direction: ASC}
-      first: 100
-      after: $endCursor
-    ) {
-      pageInfo { hasNextPage, endCursor }
-      edges {
-        node {
-          name
-          publishedAt
-          isPrerelease
-        }
-      }
-    }
-  }
-}'""" % (repo, owner), capture_output=True, timeout=300, check=True, shell=True)  # noqa: UP031
-    logging.info(f"fetched {repo_id} GitHub releases")
-
-    # splitting because response may contain multiple JSON objects on a single line
-    responses = child.stdout.decode("utf-8").strip().replace('}{', '}\n{').split("\n")
-    return [json.loads(response) for response in responses]
-
-
 p_filter = sys.argv[1] if len(sys.argv) > 1 else None
 m_filter = sys.argv[2] if len(sys.argv) > 2 else None
 for config in endoflife.list_configs(p_filter, METHOD, m_filter):
     with releasedata.ProductData(config.product) as product_data:
-        for page in fetch_releases(config.url):
-            releases = [edge['node'] for edge in (page['data']['repository']['releases']['edges'])]
+        for release in github.fetch_releases(config.url):
+            if release.is_prerelease:
+                continue
 
-            for release in releases:
-                if not release['isPrerelease']:
-                    version_str = release['name']
-                    version_match = config.first_match(version_str)
-                    if version_match:
-                        version = config.render(version_match)
-                        date = dates.parse_datetime(release['publishedAt'])
-                        product_data.declare_version(version, date)
+            version_str = release.name
+            version_match = config.first_match(version_str)
+            if version_match:
+                version = config.render(version_match)
+                date = dates.parse_datetime(release.published_at)
+                product_data.declare_version(version, date)
