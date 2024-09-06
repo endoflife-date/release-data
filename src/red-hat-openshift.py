9bb46a6d3dc5b995aec2eecf69ab43f009cf8b49
dadav <33197631+dadav@users.noreply.github.com>
2024-09-06 18:50:19 +0200

    fix: Adjust pattern to latest changes

--- src/red-hat-openshift.py
+++ src/red-hat-openshift.py
@@ -6,7 +6,7 @@ from common.git import Git
 """Fetches Red Hat OpenShift versions from the documentation's git repository"""
 
 VERSION_AND_DATE_PATTERN = re.compile(
-    r"{product-title}\s(?P<version>\d+\.\d+\.\d+).*\n+Issued:\s(?P<date>\d{4}-\d\d-\d\d)$",
+    r"{product-title}\s(?P<version>{product-version}\.\d+|\d+\.\d+\.\d+).*\n+Issued:\s(?P<date>\d\d?\s[a-zA-Z]+\s\d{4}|\d{4}-\d\d-\d\d)$",
     re.MULTILINE,
 )
 
@@ -16,8 +16,9 @@ with releasedata.ProductData("red-hat-openshift") as product_data:
 
     # only fetch v4+ branches, because the format was different in openshift v3
     for branch in git.list_branches("refs/heads/enterprise-[4-9]*"):
-        version = branch.split("-")[1].replace(".", "-")
-        release_notes_filename = f"release_notes/ocp-{version}-release-notes.adoc"
+        branch_version = branch.split("-")[1]
+        file_version = branch_version.replace(".", "-")
+        release_notes_filename = f"release_notes/ocp-{file_version}-release-notes.adoc"
         git.checkout(branch, file_list=[release_notes_filename])
 
         release_notes_file = git.repo_dir / release_notes_filename
@@ -27,4 +28,7 @@ with releasedata.ProductData("red-hat-openshift") as product_data:
         with release_notes_file.open("rb") as f:
             content = f.read().decode("utf-8")
             for version, date_str in VERSION_AND_DATE_PATTERN.findall(content):
-                product_data.declare_version(version, dates.parse_date(date_str))
+                product_data.declare_version(
+                    version.replace("{product-version}", branch_version),
+                    dates.parse_date(date_str),
+                )
