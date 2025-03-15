926980d67fe1b389516320891a765e5b423b4c1f
Max Ulidtko <ulidtko@gmail.com>
2025-03-15 16:39:18 +0100

    Update wiki-parsing script ghc-wiki.py

--- src/ghc-wiki.py
+++ src/ghc-wiki.py
@@ -1,5 +1,17 @@
+"""
+In addition to automation from git tags,
+use the GHC-status wiki page on GHC gitlab
+as a source for "Not recommended for use" end-of-support signals.
+
+References:
+    https://github.com/endoflife-date/release-data/pull/395
+    https://github.com/endoflife-date/endoflife.date/pull/6287
+    https://gitlab.haskell.org/ghc/ghc/-/wikis/GHC-status
+"""
+
 import logging
 import re
+import sys
 
 from common import dates, endoflife, http, releasedata
 
@@ -31,6 +43,7 @@ def parse_markdown_tables(lineiter):
             return # f**ing PEP 479
 
 def maybe_markdown_table_row(line):
+    line = line.strip()
     if not re.match(r'[|].*[|]', line):
         return None
     return [x.strip() for x in line.strip('|').split('|')]
@@ -43,25 +56,15 @@ def main():
         assert data['title'] == "GHC Status"
         assert data['format'] == "markdown"
         md = data['content'].splitlines()
-        #-- parse tables, here we expect three
-        [series_table, patchlevel9x, patchlevel8x] = parse_markdown_tables(iter(md))
 
-        for row in patchlevel9x[1:]:
-            [milestone, date, _ticket, _manager, _download] = row
-            version = milestone.lstrip('%')
-            date = (date # kludges for inconsistencies in handwritten dates
-                .replace("Sept", "Sep")
-                #.replace("November", "Nov")
-                #.replace("April", "Apr")
-                #.replace("July", "Jul")
-            )
-            date = dates.parse_date(date, ['%d %b %Y', '%d %B %Y'])
-            product.declare_version(version, date)
-
-        # NOTE: versions in patchlevel8x ignored by the script, all EOL at this point.
+        #-- Parse tables out of the wiki text. At time of writing, the script expects exactly two:
+        #-- 1. "Most recent major" with 5 columns
+        #-- 2. "All released versions" with 5 columns
+        [series_table, patchlevel] = parse_markdown_tables(iter(md))
 
         for row in series_table[1:]:
-            [series, mostrecent, nextplanned, status] = row
+            [series, _downloadlink, mostrecent, nextplanned, status] = row
+            series = series.split(' ') [0]
             series = series.replace('\\.', '.')
             if series == "Nightlies": continue
             mostrecent = mostrecent.lstrip('%').replace('"', '')
@@ -72,7 +75,7 @@ def main():
             #-- The clearest semblance of an EOL signal we get
             r.set_eol("not recommended for use" in status or ":red_circle:" in status)
             #-- eoasColumn label is "Further releases planned"
-            r.set_eoas(nextplanned in ("None", "N/A", ""))
+            r.set_eoas(any(keyword in nextplanned for keyword in ("None",  "N/A")))
             #-- Initial release in major series is x.y.1
             r.set_field('latest', mostrecent)
             if not mostrecent.endswith('.1'):
@@ -80,11 +83,21 @@ def main():
                 if latest:
                     r.set_field('latestReleaseDate', latest.date())
 
-# TODO also https://gitlab.haskell.org/ghc/release/release-metadata/
+        for row in patchlevel[1:]:
+            [milestone, _downloadlink, date, _ticket, _manager] = row
+            version = milestone.lstrip('%')
+            version = version.split(' ') [0]
+            date = dates.parse_date(date)
+            product.declare_version(version, date)
+
+
+# TODO at some point maybe use https://gitlab.haskell.org/ghc/release/release-metadata/
 
 try:
     main()
 except AssertionError:
     logging.exception("When parsing wiki tables")
+    sys.exit(1)
 except:
     logging.exception("Failed to refresh product data")
+    sys.exit(1)
