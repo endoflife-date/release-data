d3adaa2b774ea1e7acbedda30531acb0a6ce96f4
Marc Wrobel <marc.wrobel@gmail.com>
2025-05-31 07:58:43 +0200

    [rocky-linux] Use the published wiki sources instead of the development sources
    The development sources may contain upcoming information that are not yet true, such as upcoming release dates.

--- src/rocky-linux.py
+++ src/rocky-linux.py
@@ -1,7 +1,7 @@
 from common import dates, endoflife, http, releasedata
 
 with releasedata.ProductData("rocky-linux") as product_data:
-    response = http.fetch_url("https://raw.githubusercontent.com/rocky-linux/wiki.rockylinux.org/development/docs/include/releng/version_table.md")
+    response = http.fetch_url("https://raw.githubusercontent.com/rocky-linux/wiki.rockylinux.org/main/docs/include/releng/version_table.md")
     for line in response.text.strip().split('\n'):
         items = line.split('|')
         if len(items) >= 5 and endoflife.DEFAULT_VERSION_PATTERN.match(items[1].strip()):
