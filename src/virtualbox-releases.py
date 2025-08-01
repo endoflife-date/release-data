76ecc236072cd79b1ae9ed9568a5b978b4582add
Marc Wrobel <marc.wrobel@gmail.com>
2025-08-01 21:59:47 +0200

    split

--- src/virtualbox-releases.py
+++ src/virtualbox-releases.py
@@ -12,7 +12,6 @@ config = config_from_argv()
 with ProductData(config.product) as product_data:
     html = http.fetch_html(config.url)
 
-
     for li in html.select_one("#DownloadVirtualBoxOldBuilds + ul").find_all("li"):
         li_text = li.find("a").text.strip()
 
@@ -33,15 +32,3 @@ with ProductData(config.product) as product_data:
         eol_date_str = eol_match.group("value")
         eol_date = dates.parse_month_year_date(eol_date_str)
         release.set_eol(eol_date)
-
-    html = http.fetch_html(config.data['downloadsURL'])
-    for a in html.select("a"):
-        href = a["href"]
-
-        version_match = config.first_match(href)
-        if version_match:
-            version = config.render(version_match)
-            date_str = a.next_sibling.strip().split(" ")[0]
-            date = dates.parse_date(date_str)
-
-            product_data.declare_version(version, date)
