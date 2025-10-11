85689649e089f7c533cd42e2706867a314b5a826
Marc Wrobel <marc.wrobel@gmail.com>
2025-10-11 19:47:02 +0200

    [discourse] Remove debug logs

--- src/discourse.py
+++ src/discourse.py
@@ -15,11 +15,9 @@ with ProductData(config.product) as product_data:
         title = topic.select_one("span.link-top-line").get_text(strip=True)
         versions = config.first_match(title)
         if not versions:
-            logging.debug("Skipping %s: does not match regex", title)
             continue
 
         name = config.render(versions)
-        print(topic)
         date_str = topic.select_one("td.activity").get("title").strip()
         date_match = re.search(r"Created:\s*([A-Za-z]+\s+\d{1,2},\s+\d{4}\s+\d{1,2}:\d{2}\s+[ap]m)", date_str)
         if not date_match:
