da4c1d6e290e0b2e20ef41dfc3c251a0231f38a6
Marc Wrobel <marc.wrobel@gmail.com>
2025-10-28 22:26:11 +0100

    [ibm-aix] Use fetch_javascript_url instead of fetch_html

--- src/ibm-aix.py
+++ src/ibm-aix.py
@@ -1,11 +1,12 @@
+from bs4 import BeautifulSoup
 from common import dates, http
 from common.releasedata import ProductData, config_from_argv
 
 config = config_from_argv()
 with ProductData(config.product) as product_data:
-    html = http.fetch_html(config.url)
+    html = BeautifulSoup(http.fetch_javascript_url(config.url, wait_for="table"), "html5lib")
 
-    for release_table in html.find("div", class_="ibm-container-body").find_all("table", class_="ibm-data-table ibm-grid"):
+    for release_table in html.find_all("table"):
         for row in release_table.find_all("tr")[1:]:  # for all rows except the header
             cells = row.find_all("td")
             version = cells[0].text.strip("AIX ").replace(' TL', '.')
